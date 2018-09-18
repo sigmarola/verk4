8791af9622db54e35f4379add1ffddb0f59b3f83
sigmarola <42818933+sigmarola@users.noreply.github.com>
2018-09-18 22:48:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import urllib.request, json
 from sys import argv
 
 
-
+@route('/')
+def index():
+    redirect('/json')
 @route('/json')
 def jsonFile():
     jFile = open("file.json","r", encoding='utf-8')
     skra = json.load(jFile)
     jFile.close()
-    return template("index.tpl", gogn=skra, title='JSON')
+    return template("index.tpl", gogn=skra, title='JSON',pge=True)
 @route('/api')
 def index2():
     with urllib.request.urlopen("https://apis.is/currency") as url:
         skra = json.loads(url.read().decode())
-    return template("index.tpl", gogn=skra,title='API')
+    return template("index.tpl", gogn=skra,title='API',pge=False)
 bottle.run(host='0.0.0.0', port=argv[1])
